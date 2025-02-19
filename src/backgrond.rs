c1b67089aaeb6aa3d80003072d46cd1c01959954
Ian Marcos <ianmarcosgomesefreitas@gmail.com>
2025-02-18 23:44:53 -0300

    feat: add fear background

--- src/backgrond.rs
+++ src/backgrond.rs
@@ -3,10 +3,10 @@ use chrono::Datelike;
 
 pub fn get_day_bg() -> String {
     let now = chrono::Local::now();
-    let day = now.day();
+    let day = now.day() as usize;
     
-    const ELEMENTS: [&str; 4] = ["Sangue", "Morte", "Conhecimento", "Energia"];
+    const ELEMENTS: [&str; 5] = ["Sangue", "Morte", "Conhecimento", "Energia", "Medo"];
 
-    let element_idx = day % 4;
-    format!("{}.jpeg", ELEMENTS[element_idx as usize])
+    let element_idx = day % ELEMENTS.len();
+    format!("{}.jpeg", ELEMENTS[element_idx])
 }
