22039474245b690b958c465b726722dbe4ef743e
Ian Marcos <ianmarcosgomesefreitas@gmail.com>
2025-03-26 23:16:57 -0300

    refactor: make episode list a global reference and add 'Natal Macabro' episodes

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,51 +1,47 @@
+use api::episode_history;
 use axum::{
     http::{HeaderValue, Method},
     routing::get,
     Router,
 };
 
-use common::{episode::Episode, EPISODES_JSON};
+use common::episode::Episode;
 use rand::Rng;
 use tower_http::cors::{Any, CorsLayer};
 
 use std::collections::HashSet;
 
 pub async fn get_day_episode(episodes: &[Episode]) -> usize {
-    let used_eps = api::episode_history::get_episode_idx_history().await.unwrap();
+    let invalid_eps = episode_history::get_episode_idx_history().await.unwrap();
 
-    let opt_registered_ep = used_eps.iter().find(|entry| entry.day == common::get_day_offset());
+    let opt_registered_ep = invalid_eps.iter().find(|entry| entry.day == common::get_day_offset());
     if let Some(registered_ep) = opt_registered_ep {
         return registered_ep.episode_idx;
     }
 
-    let unused_eps: Vec<usize> = {
-        let used_idxs = used_eps.iter().map(|entry| entry.episode_idx).collect::<HashSet<usize>>();
+    let invalid_eps: Vec<usize> = {
+        let invalid_idxs = invalid_eps.iter().map(|entry| entry.episode_idx).collect::<HashSet<usize>>();
         let set = (0..episodes.len()).collect::<HashSet<usize>>();
-        set.difference(&used_idxs).cloned().collect::<Vec<usize>>()
+        set.difference(&invalid_idxs).cloned().collect::<Vec<usize>>()
     };
     
-    let mapped_idx = rand::rng().random_range(0..unused_eps.len());
-    let episode_idx = unused_eps[mapped_idx];
+    let mapped_idx = rand::rng().random_range(0..invalid_eps.len());
+    let episode_idx = invalid_eps[mapped_idx];
 
-    api::episode_history::register_day_episode(episode_idx).await.unwrap();
+    episode_history::register_day_episode(episode_idx).await.unwrap();
 
     episode_idx
 }
 
 async fn day_episode() -> String {
-    let episodes: Vec<Episode> = serde_json::from_str(EPISODES_JSON).unwrap();
-    let episode_idx: usize = get_day_episode(&episodes).await;
+    let episode_idx: usize = get_day_episode(&common::EPISODES_LIST).await;
 
     episode_idx.to_string()
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let cors_origin = if cfg!(debug_assertions) {
-        "http://127.0.0.1:8080"
-    } else {
-        "https://ianmgf.github.io"
-    };
+    let cors_origin = env!("CORS_ORIGIN");
 
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,8 +1,12 @@
+use std::sync::Arc;
+
+use episode::Episode;
+use lazy_static::lazy_static;
 use chrono::NaiveDate;
 
 pub mod episode;
 
-pub const NON_REPEATING_PERIOD: u64 = 64;
+pub const NON_REPEATING_PERIOD: u64 = 30;
 pub const EPISODES_JSON: &str = include_str!("../data/episodes.json");
 
 pub fn get_day_offset() -> usize {
@@ -10,4 +14,8 @@ pub fn get_day_offset() -> usize {
     let curr_date = chrono::Local::now().date_naive();
 
     curr_date.signed_duration_since(day_zero).num_days() as usize
+}
+
+lazy_static! {
+    pub static ref EPISODES_LIST: Arc<Vec<Episode>> = Arc::new(serde_json::from_str(EPISODES_JSON).unwrap());
 }
